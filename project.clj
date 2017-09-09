80f82cfd73f54d88fa9109075ca1ad8618d96c29
EAS7510 <michaeleaster@northwesternmutual.com>
2017-09-09 18:47:56 -0500

    Snapshot.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.mike706574/pancake "0.0.1"
+(defproject org.clojars.mike706574/pancake "0.0.2-SNAPSHOT"
   :description "Fixed-width file parsing library."
   :url "https://github.com/mike706574/pancake"
   :license {:name "Eclipse Public License"
