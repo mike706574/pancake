1886d43613865090232bb7cad7eccbf863278536
EAS7510 <michaeleaster@northwesternmutual.com>
2017-10-16 16:40:57 -0500

    Version 0.0.19-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fun.mike/pancake "0.0.18"
+(defproject fun.mike/pancake "0.0.19-SNAPSHOT"
   :description "Flat file data parsing library."
   :url "https://github.com/mike706574/pancake"
   :license {:name "Eclipse Public License"
