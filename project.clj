1991819366342fda5abfc5dfda1ddc1ff863e8e1
EAS7510 <michaeleaster@northwesternmutual.com>
2017-10-16 14:19:06 -0500

    Release to clojars.

--- project.clj
+++ project.clj
@@ -10,4 +10,6 @@
                    :dependencies [[org.clojure/clojure "1.9.0-beta2"]
                                   [org.clojure/tools.namespace "0.2.11"]
                                   [org.clojure/test.check "0.10.0-alpha2"]]}}
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :creds :gpg}]]
   :repl-options {:init-ns user})
